0ffb49c70c92ef39a41203ce51ea325a7d41377c
Esteban Cortina <estebannsebastian@gmail.com>
2023-08-04 14:35:33 -0600

    Feature get & post products

--- routes/products.py
+++ routes/products.py
@@ -1,13 +1,73 @@
+from flask import request, jsonify
 from routes import products_bp
 from config.db_config import get_connection
 
 
 @products_bp.route('/products', methods=['GET'])
-def get_users():
+def get_products():
     mysqlConn = get_connection()
-    users = [
-        {'id': 1, 'name': 'Usuario 1'},
-        {'id': 2, 'name': 'Usuario 2'},
-        {'id': 3, 'name': 'Usuario 3'}
-    ]
-    return users
+    cursor = mysqlConn.cursor()
+    cursor.execute('SELECT * FROM products')
+    categories = cursor.fetchall()
+    cursor.close()
+    mysqlConn.close()
+    return categories
+
+
+@products_bp.route('/products/<int:id>', methods=['POST'])
+def post_product(id):
+    if (request.is_json):
+        data = request.get_json()
+        name = data.get('name')
+        description = data.get('description')
+        category_id = data.get('category_id')
+        created_by = id
+        mysqlConn = get_connection()
+        cursor = mysqlConn.cursor()
+        query = f'''
+        INSERT INTO products(
+          name, description,
+          category_id, 
+          created_at, created_by) 
+          VALUES('{name}','{description}',
+          {category_id},
+          NOW(),{created_by})
+        '''
+        try:
+            cursor.execute(query)
+            mysqlConn.commit()
+            return jsonify({'message': 'producto insertado'}), 200
+        except Exception as e:
+            print(e)
+            mysqlConn.rollback()
+            return jsonify({'message': 'Error categoria no insertada'+e}), 400
+        finally:
+            cursor.close()
+            mysqlConn.close()
+    else:
+        name = request.form.get('name')
+        description = request.form.get('description')
+        category_id = request.form.get('category_id')
+        created_by = id
+        mysqlConn = get_connection()
+        cursor = mysqlConn.cursor()
+        query = f'''
+        INSERT INTO products(
+          name, description,
+          category_id, 
+          created_at, created_by) 
+          VALUES('{name}','{description}',
+          {category_id},
+          NOW(),{created_by})
+        '''
+        try:
+            cursor.execute(query)
+            mysqlConn.commit()
+            return jsonify({'message': 'Producto insertada'}), 200
+        except Exception as e:
+            print(e)
+            mysqlConn.rollback()
+            return jsonify({'message': 'Error categoria no insertada'+e}), 400
+        finally:
+            cursor.close()
+            mysqlConn.close()
